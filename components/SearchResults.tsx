3ae1ba11c74916f25a5b1e4d5d239eaca897a672
Zenky <bzenky@gmail.com>
2022-03-06 09:59:04 -0300

    adding memo

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -20,3 +20,16 @@ export function SearchResults({ results }: SearchResultsProps) {
         </div>
     )
 }
+
+/* Atualização
+* 1. Criar uma nova versão do componente
+* 2. Comparar com a versão anterior
+* 3. Se houverem alterações, vai atualizar o que alterou
+*/
+
+/* Quando usar o memo?
+* 1. Pure Functional Components
+* 2. Renders too often
+* 3. Re-renders with same props
+* 4. Medium to big size
+*/
